2b4be37d5e0d72b98d4c146a317e1f8e6bc6ba36
Panupatr Limprasert <jamespanupatr@gmail.com>
2023-10-28 23:54:21 +0800

    :sparkles: Simple Damageable base class for enemy

---
+++ Assets/Scripts/Entity/Damageable.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class Damageable : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField]
+    protected int maxHP;
+    [SerializeField]
+    protected int hp;
+
+    [Header("Effects")]
+    public UnityEvent onDamage;
+
+    protected virtual void Start()
+    {
+        hp = maxHP;
+    }
+
+    ///<summary>Call by OnCollision2D of the attacking gameobject after checking layer or type of Damageable</summary>
+    public virtual bool TakeDamage(int damage)
+    {
+        bool killShot = false;
+
+        if (hp - damage <= 0) killShot = true;
+
+        ApplyHP(-damage);
+        onDamage.Invoke();
+
+        return killShot;
+    }
+
+    public virtual void ApplyHP(int change)
+    {
+        hp += change;
+
+        if (hp <= 0) OnDead();
+    }
+
+    public virtual void OnDead()
+    {
+        Destroy(this.gameObject);
+    }
+}
