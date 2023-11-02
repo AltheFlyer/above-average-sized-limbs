b4a7ac6ae672bd595f9f0ae8b5fd60f57f51c945
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2023-11-02 17:41:07 +0800

    Moving between rooms

--- Assets/Scripts/Office/Door.cs
+++ Assets/Scripts/Office/Door.cs
@@ -1,8 +1,56 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-/// WIP
+// Doors transport the player to different rooms upon entry
 public class Door : MonoBehaviour
 {
-    // What room to send the player to
+    // The position of the room to send the player to
     public Vector2Int destination;
+    public Vector2Int direction;
+
+    public UnityEvent<Vector2Int, Vector2Int> onEnterCallback;
+
+
+    // Internal state
+    private bool isOpen;
+
+    // Internal components
+    private SpriteRenderer spriteRenderer;
+
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("You didn't attach a sprite to the door!");
+        }
+    }
+
+    public void SetOpenState(bool open)
+    {
+        isOpen = open;
+        if (open)
+        {
+            spriteRenderer.enabled = true;
+        }
+        else
+        {
+            spriteRenderer.enabled = false;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // TODO: Better mechanism to check for player collision
+        if (other.GetComponent<PlayerManager>() == null)
+        {
+            return;
+        }
+        if (!isOpen)
+        {
+            return;
+        }
+
+        onEnterCallback.Invoke(destination, direction);
+    }
 }
--- Assets/Scripts/Office/OfficeExplorationController.cs
+++ Assets/Scripts/Office/OfficeExplorationController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class OfficeExplorationController : MonoBehaviour
 {
@@ -10,9 +11,20 @@ public class OfficeExplorationController : MonoBehaviour
 
     private Room currentRoom;
 
+    public Door[] doors;
+
+    public UnityEvent<Vector2Int, Vector2Int> onRoomChange;
+
+    public Camera camera;
+
     public void Start()
     {
         officeMap = generator.GenerateMap();
+
+        for (int i = 0; i < doors.Length; ++i)
+        {
+            doors[i].onEnterCallback = onRoomChange;
+        }
 
         // Monkey: for each room, instantiate it and then disable it.
         for (int y = 0; y < officeMap.rooms.Length; ++y)
@@ -38,13 +50,67 @@ public class OfficeExplorationController : MonoBehaviour
         currentRoom = officeMap.GetRoom(new Vector2Int(5, 5));
 
         currentRoom.Activate();
+
+        for (int i = 0; i < doors.Length; ++i)
+        {
+            if (officeMap.HasRoom(new Vector2Int(5, 5) + doors[i].direction))
+            {
+                doors[i].gameObject.SetActive(true);
+                doors[i].destination = new Vector2Int(5, 5) + doors[i].direction;
+                doors[i].SetOpenState(true);
+            }
+            else
+            {
+                doors[i].SetOpenState(false);
+            }
+        }
     }
 
     public void ChangeRoom(Vector2Int newPos, Vector2Int direction)
     {
-        currentRoom.Deactivate();
+        Debug.Log($"Moving to room at {newPos}");
+        // Deactivate current room
+        // currentRoom.Deactivate();
 
+        // Activate new room
         currentRoom = officeMap.GetRoom(newPos);
-        currentRoom.Activate();
+        // currentRoom.Activate();
+
+        // Prepare doors
+        for (int i = 0; i < doors.Length; ++i)
+        {
+            if (officeMap.HasRoom(newPos + doors[i].direction))
+            {
+                doors[i].destination = newPos + doors[i].direction;
+                doors[i].SetOpenState(true);
+            }
+            else
+            {
+                doors[i].SetOpenState(false);
+            }
+        }
+
+        // Move everything important to the next room
+        // Move the player to the appropriate entrance
+        GameObject.FindObjectOfType<PlayerManager>().transform.position += new Vector3(
+            0,
+            0,
+            0
+        );
+        // Move the camera
+        camera.transform.position = new Vector3(
+            newPos.x * 13 - 5 * 13,
+            newPos.y * 7 - 5 * 7,
+            -10
+        );
+        // Move the doors (lol)
+        for (int i = 0; i < doors.Length; ++i)
+        {
+            doors[i].transform.position += new Vector3(
+                direction.x * 13,
+                direction.y * 7,
+                0
+            );
+        }
     }
 }
