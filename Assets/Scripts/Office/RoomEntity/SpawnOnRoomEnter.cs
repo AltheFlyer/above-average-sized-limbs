536a620372949de789eb69d9dc33540f307ed4af
Mark <miniapple8888@gmail.com>
2023-11-09 03:01:05 -0500

    Change enemy type to make rooms work

--- Assets/Scripts/Office/RoomEntity/SpawnOnRoomEnter.cs
+++ Assets/Scripts/Office/RoomEntity/SpawnOnRoomEnter.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SpawnOnRoomEnter : RoomActivatable
 {
+    public static List<GameObject> enemyList = new List<GameObject>();
     [Header("Prefab for the thing to spawn when the room is entered for the first time.")]
     public GameObject spawningPrefab;
 
     public override void OnRoomFirstEnter()
     {
         // Spawns an enemy as a child of the spawner object
-        Instantiate(spawningPrefab, transform.position, Quaternion.identity, transform);
+        enemyList.Add(Instantiate(spawningPrefab, transform.position, Quaternion.identity, transform));
+    }
+
+    public static bool EnemyListIsEmpty()
+    {
+        enemyList = enemyList.Where(enemy => enemy != null).ToList();
+        return enemyList.Count == 0;
     }
 }
