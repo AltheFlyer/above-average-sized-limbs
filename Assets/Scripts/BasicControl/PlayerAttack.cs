b511221c7cb8ed737929ccb92b70e9887e8a5087
Huy Nguyen <edwardhaythamconnor@gmail.com>
2023-11-14 18:19:20 +0800

    Player can damage Enemy

--- Assets/Scripts/BasicControl/PlayerAttack.cs
+++ Assets/Scripts/BasicControl/PlayerAttack.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private Collider2D hitBox;
-    // Start is called before the first frame update
+    public LayerMask hitMask;
     void Start()
     {
         hitBox = GetComponent<Collider2D>();
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        if (IsMaskMatched(hitMask, col.gameObject))
         {
+            Debug.Log("Hit Enemy");
             GlobalEventHandle.instance?.onHit.Raise(new HitData());
-            col.gameObject.GetComponent<Damageable>().TakeDamage(1);
 
+
+            col.gameObject.GetComponent<Damageable>().TakeDamage(1);
         }
 
+    }
+
+    public virtual bool IsMaskMatched(LayerMask mask, GameObject target)
+    {
+        return ((mask.value & (1 << target.layer)) > 0);
     }
 }
