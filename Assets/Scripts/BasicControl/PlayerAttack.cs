f3b485d3df67abbb7cc6692f84ef1eab14954423
Huy Nguyen <edwardhaythamconnor@gmail.com>
2023-11-15 00:58:35 +0800

    Add Basic Combo System

--- Assets/Scripts/BasicControl/PlayerAttack.cs
+++ Assets/Scripts/BasicControl/PlayerAttack.cs
@@ -6,26 +6,54 @@ public class PlayerAttack : MonoBehaviour
 {
     private Collider2D hitBox;
     public LayerMask hitMask;
+    public IntVariable comboCount;
+    private int comboTimeLimit = 1000000;
+    public IntVariable comboTimer;
+
     void Start()
     {
         hitBox = GetComponent<Collider2D>();
+        comboCount.SetValue(0);
+        comboTimer.SetValue(comboTimeLimit);
+    }
+
+    void Update()
+    {
+        // Update the combo timer
+        comboTimer.ApplyChange(-1);
+
+        // Check if the combo timer has reached zero
+        if (comboTimer.Value <= 0)
+        {
+            // Reset the combo if the timer has elapsed
+            comboCount.SetValue(0);
+            // Reset the timer
+            ResetComboTimer();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (IsMaskMatched(hitMask, col.gameObject))
         {
-            Debug.Log("Hit Enemy");
+            comboCount.ApplyChange(1);
+            Debug.Log(comboCount.Value);
+            ResetComboTimer();
             GlobalEventHandle.instance?.onHit.Raise(new HitData());
 
-
             col.gameObject.GetComponent<Damageable>().TakeDamage(1);
-        }
 
+        }
+    }
+
+    void ResetComboTimer()
+    {
+        comboTimer.SetValue(comboTimeLimit);
     }
 
     public virtual bool IsMaskMatched(LayerMask mask, GameObject target)
     {
         return ((mask.value & (1 << target.layer)) > 0);
     }
+
 }
