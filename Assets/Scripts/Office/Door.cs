510f36a4f36f0bff2041198a486672de1c07ec6d
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2023-11-23 16:11:14 +0800

    Fix wall and door colliders

--- Assets/Scripts/Office/Door.cs
+++ Assets/Scripts/Office/Door.cs
@@ -14,28 +14,13 @@ public class Door : MonoBehaviour
     // Internal state
     private bool isOpen; // isOpen means door is openable and not just a wall (can be locked tho)
 
-    // Internal components
-    public SpriteRenderer spriteRenderer;
-
     public void Start()
     {
-        if (spriteRenderer == null)
-        {
-            Debug.LogError("You didn't attach a sprite to the door!");
-        }
     }
 
     public void SetOpenState(bool open)
     {
         isOpen = open;
-        if (open)
-        {
-            spriteRenderer.enabled = true;
-        }
-        else
-        {
-            spriteRenderer.enabled = false;
-        }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -56,11 +41,13 @@ public class Door : MonoBehaviour
 
     public void SetDoorLock(bool lockState)
     {
-        // Spot if this door has 1 child (it should) - locking doors
+        // ENable/disable children
         if (transform.childCount > 0)
         {
-            Transform doorCollider = transform.GetChild(0);
-            doorCollider.gameObject.SetActive(lockState || !isOpen);
+            foreach (Transform t in transform)
+            {
+                t.gameObject.SetActive(lockState || !isOpen);
+            }
         }
         else
         {
