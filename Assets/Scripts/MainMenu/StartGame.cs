7a59c598e97770b46d2608acdfacc8bdbba92b7f
James Fan <jmfan2002@gmail.com>
2023-11-28 17:03:59 +0800

    main menu screen

---
+++ Assets/Scripts/MainMenu/StartGame.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    int LEVEL_1_SCENE_BUILD_INDEX = 1;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(LEVEL_1_SCENE_BUILD_INDEX);
+    }
+}
