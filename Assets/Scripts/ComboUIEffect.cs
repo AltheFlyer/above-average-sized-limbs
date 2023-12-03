fd75f22a0e487ee7928b62548046bb061e8a0987
Huy Nguyen <edwardhaythamconnor@gmail.com>
2023-12-04 02:18:13 +0800

    Fix combo positions

--- Assets/Scripts/ComboUIEffect.cs
+++ Assets/Scripts/ComboUIEffect.cs
@@ -2,71 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+//using UnityEngine.Rendering.PostProcessing;
 
 public class ComboUIEffect : MonoBehaviour
 {
     public IntVariable comboCount;
-    // public GameObject comboText;
-    // private GameObject currentComboText;
-    //public GameObject comboText;
     public TextMeshProUGUI comboText;
     private bool isTextShaking = false;
-    private float jitterAmount;
-    private float jitterSpeed;
+    public float[] baseJitterParams;
+    private float[] activeJitterParams;
     private Vector3 originalPosition;
+    private int[] comboThreshold = new int[2];
+
     void Start()
     {
         //currentComboText = Instantiate(comboText, this.transform.position, Quaternion.identity, transform);
         comboText.gameObject.SetActive(true);
-        originalPosition = comboText.transform.position;
+        originalPosition = comboText.transform.localPosition;
+        activeJitterParams = new float[2];
+        comboThreshold[0] = 3;
+        comboThreshold[1] = 6;
     }
     void Update()
     {
-
         if (isTextShaking)
         {
-            float jitterX = Mathf.PerlinNoise(Time.time * jitterSpeed, 0) * 2 - 1;
-            float jitterY = Mathf.PerlinNoise(0, Time.time * jitterSpeed) * 2 - 1;
-
-            Vector3 jitterOffset = new Vector3(jitterX, jitterY, 0) * jitterAmount;
-            comboText.transform.position = originalPosition + jitterOffset;
+            ApplyJitter(activeJitterParams[0], activeJitterParams[1]);
         }
-
         ShowComboEffect();
     }
 
     public void ShowComboEffect()
     {
-        Color comboTextColor = Color.white;
+        Color comboTextColor;
 
-        if (comboCount.Value <= 1)
+        if (comboCount.Value <= comboThreshold[0])
         {
             comboTextColor = Color.white;
             isTextShaking = false;
-            jitterAmount = 0f;
-            jitterSpeed = 0f;
+            comboText.transform.localPosition = originalPosition;
         }
-        else if (comboCount.Value <= 3 && comboCount.Value > 1)
+        else if (comboCount.Value <= comboThreshold[1] && comboCount.Value > comboThreshold[0])
         {
             comboTextColor = Color.yellow;
             isTextShaking = true;
-            jitterAmount = 4f;
-            jitterSpeed = 20f;
+            activeJitterParams[0] = baseJitterParams[0];
+            activeJitterParams[1] = baseJitterParams[1];
         }
         else
         {
             comboTextColor = Color.red;
             isTextShaking = true;
-            jitterAmount = 6f;
-            jitterSpeed = 30f;
+            activeJitterParams[0] = baseJitterParams[0] * 5;
+            activeJitterParams[1] = baseJitterParams[1] * 5;
         }
-
 
         comboText.color = comboTextColor;
         comboText.text = "Combo: " + comboCount.Value.ToString();
     }
 
-    void ApplyJitter()
+    void ApplyJitter(float jitterAmount, float jitterSpeed)
     {
         float jitterX = Mathf.PerlinNoise(Time.time * jitterSpeed, 0) * 2 - 1;
         float jitterY = Mathf.PerlinNoise(0, Time.time * jitterSpeed) * 2 - 1;
@@ -75,13 +70,3 @@ public class ComboUIEffect : MonoBehaviour
         comboText.transform.localPosition = originalPosition + jitterOffset;
     }
 }
-
-//     private void RotateText(GameObject textObject, float angle, float maxAngle)
-//     {
-//         //textObject.transform.Rotate(Vector3.forward, angle);
-//         while (textObject.transform.rotation.z <= maxAngle)
-//         {
-//             textObject.transform.Rotate(Vector3.forward, angle);
-//         }
-//     }
-// }
